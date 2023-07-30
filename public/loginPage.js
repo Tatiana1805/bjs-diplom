b535a178f5bc6e9bb76177562928f4b6054988e5
Tatiana1805 <tycr2020@gmail.com>
2023-07-30 15:12:51 +0300

    Задания выполнены

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,25 +1,48 @@
-"use strict"
-class UserForm {
-    constructor(){
+// "use strict"
+// class UserForm {
+//     constructor(){
 
+//     }
+//     loginFormCallback (data = {login, password}){//функция, которая будет выполняться при попытке авторизации
+//         if(!data.login && !data.password){
+//             alert ('Введите правильный логин и пароль')
+//     } 
+//     return this.loginFormCallback;
+// }
+//     registerFormCallback (){ // функция, которая будет выполняться при попытке регистрации
+//         if(!!login || !!password){
+//         return this.registerFormCallback
+//         }
+//         else {
+//         alert ('Введите логин и пароль')
+// }
+// }
+// }
+// class ApiConnector {
+//     constructor(){}
+//     login({login, password}, callback)
+//     register({login, password}, callback)
+// }
+"use strict"
+
+const userForm = new UserForm();
+// Авторизация
+userForm.loginFormCallback = (data = {login: null, password: null}) => {
+  ApiConnector.login(data, (response)=>{
+    if (response.success) {
+      location.reload();
+    } else {
+      alert(response.data);
     }
-    loginFormCallback (data = {login, password}){//функция, которая будет выполняться при попытке авторизации
-        if(!data.login && !data.password){
-            alert ('Введите правильный логин и пароль')
-    } 
-    return this.loginFormCallback;
+  })
 }
-    registerFormCallback (){ // функция, которая будет выполняться при попытке регистрации
-        if(!!login || !!password){
-        return this.registerFormCallback
-        }
-        else {
-        alert ('Введите логин и пароль')
-}
-}
-}
-class ApiConnector {
-    constructor(){}
-    login({login, password}, callback)
-    register({login, password}, callback)
+// Регистрация
+userForm.registerFormCallback = (data = {login: null, password: null}) => {
+  ApiConnector.register(data, (response)=>{
+    if (response.success) {
+      location.reload();
+    } else {
+      alert(response.data);
+    }
+  })
 }
